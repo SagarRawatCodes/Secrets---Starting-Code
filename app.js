e59fc9a68fbdb47f83317c293681fd21da2debd5
sagar singh rawat <sagarrwt845@gmail.com>
2023-09-23 01:30:06 -0400

    hello world

---
+++ app.js
@@ -0,0 +1,73 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+console.log(process.env.API_KEY);
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", async function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    try {
+        await newUser.save();
+        res.render('secrets');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/register'); // Redirect back to the registration page in case of an error.
+    }
+});
+app.post("/login", async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const foundUser = await User.findOne({ email: username }).exec();
+
+        if (foundUser && foundUser.password === password) {
+            res.render('secrets');
+        } else {
+            res.redirect('/login'); // Redirect back to the login page if authentication fails.
+        }
+    } catch (err) {
+        console.log(err);
+        res.redirect('/login'); // Redirect back to the login page in case of an error.
+    }
+});
+app.listen(1000, function () {
+    console.log("Server is running at port 1000");
+});
